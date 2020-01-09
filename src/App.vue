64a3727c6e391db7682305a76639e48ee09b5676
KriXPello <krixpello@gmail.com>
2020-01-09 22:40:18 +0300

    ready

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+
+    <div id="wrapper" ref="wrapper">
+      <image-block
+        v-for="(image, i) in images"
+        :key="i"
+        :src="image"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import ImageBlock from './components/ImageBlock.vue';
+
+const getters = [
+  'images',
+];
+
+export default {
+  name: 'App',
+  components: {
+    'image-block': ImageBlock,
+  },
+  computed: {
+    ...mapGetters(getters),
+  },
+  mounted() {
+    const optimize = () => {
+      const wrapper = this.$refs.wrapper;
+
+      const width = Math.floor(window.innerWidth / 300) * 300;
+
+      wrapper.setAttribute('style', `width: ${width}px`);
+    }
+
+    window.onresize = optimize;
+
+    optimize();
+  }
+}
+</script>
+
+<style>
+html {
+  font-size: 16px;
+  font-family: 'Open Sans', sans-serif;
+  color: #1F0A09;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: #fff;
+  overflow-x: hidden;
+}
+
+#app {
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#wrapper {
+  display: inline-block;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/ImageBlock.vue
@@ -0,0 +1,54 @@
+<template>
+  <a id="image-block" :href="src">
+
+    <img :src="src" alt="изображение" class="image">
+
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ImageBlock',
+  props: {
+    src: String,
+  },
+  data: () => {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+#image-block {
+  width: 280px;
+  height: 200px;
+  margin: 10px;
+}
+
+.image {
+  width: 100%;
+  max-width: 280px;
+  height: 100%;
+}
+
+#image-block:hover {
+  transition: all 300ms;
+  transform: scale(1.1);
+}
+
+#image-block:active {
+  transition: all 100ms;
+  transform: scale(1.05);
+}
+
+@media (max-width: 600px) {
+  #image-block {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
